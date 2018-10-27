c1b3201f24fb57309e211dd77c82461f140f9538
yuri grinshteyn <ygrinshteyn@google.com>
2018-10-26 21:37:26 -0700

    cutting down to single go file

--- goweb.go
+++ goweb.go
@@ -7,10 +7,12 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Output(1, "request made: "+r.URL.Path)
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	log.Output(1, "web server is up")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
